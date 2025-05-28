cec2aff81dcffdd3d5b117ad39ae24ab1c9cf888
ImmortalIdiot <max.krasulya@yandex.ru>
2025-05-29 01:17:01 +0300

    fix: correct shard indexes mapping in connection string resolver

--- src/main/kotlin/com/immortalidiot/sharding/Sharding.kt
+++ src/main/kotlin/com/immortalidiot/sharding/Sharding.kt
@@ -12,7 +12,7 @@ object ShardManager {
     fun getConnectionString(shardIndex: Int): String {
         return when (shardIndex) {
             1 -> JDBCUrl.FIRST_SHARD_URL
-            2 -> JDBCUrl.SECOND_SHARD_URL
+            0 -> JDBCUrl.SECOND_SHARD_URL
             else -> throw IllegalArgumentException("Invalid shard index")
         }
     }
